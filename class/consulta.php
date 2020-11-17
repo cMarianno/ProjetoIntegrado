9bf89659dc0491dd7bc3a5b55e5d523d081578d5
cMarianno <59261601+cMarianno@users.noreply.github.com>
2020-11-17 16:26:57 -0300

    Add consulta Class

---
+++ class/consulta.php
@@ -0,0 +1,30 @@
+<?php
+class Consulta {
+    private $local = "";
+    private $horario = "";
+
+    public function getters(){
+
+    }
+
+    public function setters(){
+        
+    }
+
+    public function incluirConsulta(){
+        
+    }
+
+    public function editarConsulta(){
+        
+    }
+
+    public function excluirConsulta(){
+        
+    }
+
+    public function alertaHorarioConsulta(String $horario){
+
+        return "";
+    }
+}
